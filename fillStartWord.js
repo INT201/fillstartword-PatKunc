e38b1535dfca684abaaeea829be22a68c99c8e1e
Pattarakorn Kuncharoen <pattarakorn.kunc@kmutt.ac.th>
2022-09-07 16:36:30 +0700

    Yeahh!! 2

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,19 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  // if (word =! null || word =! undefined) {
+  //   return `"${startWord}+${word}"`
+  // }else(word= null,undefined){
+  //   return
+  // } 
+  if (word == null||word == undefined) {
+    return undefined
+  } else if(word.includes(startWord)) {
+    return word
+  } else  {
+    return startWord + word
+  }
+  
 }
-module.exports = fillStartWord
+//module.exports = fillStartWord
